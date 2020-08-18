ba4fd281d5fdbc8692e57a7bd303a6a6cc1c6fd5
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-18 12:21:17 +0100

    feat: add WeatherList component

---
+++ src/components/WeatherList/WeatherList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./WeatherList.module.scss";
+
+const WeatherList = () => {
+  return (
+    <>
+      <p>WeatherList works</p>
+    </>
+  )
+}
+
+export default WeatherList;
