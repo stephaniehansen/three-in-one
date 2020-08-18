5793da458eee4bbedb783f667c1a8714bed7e025
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-18 11:52:24 +0100

    feat: add data files

---
+++ src/data/weather.js
@@ -0,0 +1,126 @@
+const weather = [
+  {
+    "cnt": 3,
+    "list": [
+        {
+            "coord": {
+                "lon": -0.09,
+                "lat": 51.51
+            },
+            "sys": {
+                "country": "GB",
+                "timezone": 3600,
+                "sunrise": 1597726279,
+                "sunset": 1597778236
+            },
+            "weather": [
+                {
+                    "id": 803,
+                    "main": "Clouds",
+                    "description": "broken clouds",
+                    "icon": "04d"
+                }
+            ],
+            "main": {
+                "temp": 21.29,
+                "feels_like": 19.4,
+                "temp_min": 20.56,
+                "temp_max": 21.67,
+                "pressure": 1009,
+                "humidity": 64
+            },
+            "visibility": 10000,
+            "wind": {
+                "speed": 4.6,
+                "deg": 240
+            },
+            "clouds": {
+                "all": 75
+            },
+            "dt": 1597745463,
+            "id": 2643744,
+            "name": "City of London"
+        },
+        {
+            "coord": {
+                "lon": -82.19,
+                "lat": 36.6
+            },
+            "sys": {
+                "country": "US",
+                "timezone": -14400,
+                "sunrise": 1597747754,
+                "sunset": 1597796163
+            },
+            "weather": [
+                {
+                    "id": 800,
+                    "main": "Clear",
+                    "description": "clear sky",
+                    "icon": "01n"
+                }
+            ],
+            "main": {
+                "temp": 15.21,
+                "feels_like": 16.05,
+                "temp_min": 15,
+                "temp_max": 15.56,
+                "pressure": 1018,
+                "humidity": 93
+            },
+            "visibility": 10000,
+            "wind": {
+                "speed": 0.65,
+                "deg": 107
+            },
+            "clouds": {
+                "all": 1
+            },
+            "dt": 1597745605,
+            "id": 4749005,
+            "name": "Bristol"
+        },
+        {
+            "coord": {
+                "lon": -85.16,
+                "lat": 34.26
+            },
+            "sys": {
+                "country": "US",
+                "timezone": -14400,
+                "sunrise": 1597748672,
+                "sunset": 1597796671
+            },
+            "weather": [
+                {
+                    "id": 800,
+                    "main": "Clear",
+                    "description": "clear sky",
+                    "icon": "01n"
+                }
+            ],
+            "main": {
+                "temp": 18.8,
+                "feels_like": 20.29,
+                "temp_min": 17.78,
+                "temp_max": 20,
+                "pressure": 1015,
+                "humidity": 88
+            },
+            "visibility": 10000,
+            "wind": {
+                "speed": 1.14,
+                "deg": 18
+            },
+            "clouds": {
+                "all": 1
+            },
+            "dt": 1597745598,
+            "id": 4219762,
+            "name": "Rome"
+        }
+    ]
+  }
+]
+
+export default weather;
