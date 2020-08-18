fc8a017c17a350d7e90cbf252ead8a21a10c2ba5
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-18 12:21:02 +0100

    feat: add WeatherCard component

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./WeatherCard.module.scss";
+
+const WeatherCard = () => {
+  return (
+    <p>WeatherCard works</p>
+  )
+}
+
+export default WeatherCard;
