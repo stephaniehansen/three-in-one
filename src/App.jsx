e9aadc19f3ddbc50d1b7d8ffe81234accb8e0ac4
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-19 15:35:54 +0100

    feat: fetch weather data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./App.module.scss";
-import weatherData from "./data/weather";
 
 import WeatherList from "./components/WeatherList";
 
 const App = () => {
-  const [weather, setWeather] = useState(weatherData);
+  const [weather, setWeather] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchWeather = () => {
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    fetch(`http://api.openweathermap.org/data/2.5/group?id=2643744,4749005,4219762&units=metric&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(weather =>  {
+      setWeather(weather)
+      setIsLoading(false);
+    })
+    .catch(error => console.log(error));
+  }
+
+  useEffect(() => {
+    fetchWeather();
+  }, [])
 
   return (
     <>
-      <WeatherList weather={weather}/>
+      {isLoading ? "Loading" : <WeatherList weather={weather}/>}
     </>
   );
 }
