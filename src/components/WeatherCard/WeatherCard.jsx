67b13cfe19af7f4b93762f7ea9f581dfd0b7e172
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-19 13:44:29 +0100

    feat: display weather information

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from "./WeatherCard.module.scss";
 
-const WeatherCard = () => {
+const WeatherCard = ({ city }) => {
   return (
-    <p>WeatherCard works</p>
+    <>
+      <h2>{city.name}</h2>
+      <p>Temperature: {city.main.temp}</p>
+      <p>Humidity: {city.main.humidity}</p>
+      <p>Current Min Temperature: {city.main.temp_max}</p>
+      <p>Current Max Temperature: {city.main.temp_min}</p>
+    </>
   )
 }
 
