fbe5afe990037168f2c4e787377d27344a31d70f
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-19 19:22:29 +0100

    feat: add quiz - validate user input

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from "./Quiz.module.scss";
 
 const Quiz = () => {
+  const ANSWER = "pizza";
+  const [userAnswer, setUserAnswer] = useState();
+  const [result, setResult] = useState(null);
+
+  const submit = e => {
+    e.preventDefault();
+    setResult(userAnswer === ANSWER ? true : false)
+  }
+
+  const getResult = () => result ? "Correct!" : "Incorrect, try again!";
+
   return (
-    <p>Quiz Works</p>
+    <>
+      <h1>Quiz</h1>
+      <form>
+        <p>What is an Italian dish with a flat dough base called?</p>
+        <input type="text" name="answer" id="answer" onInput={e => setUserAnswer(e.target.value.toLowerCase())} />
+        <input type="submit" value="Submit" onClick={submit}/>
+      </form>
+      <p>{result !== null ? getResult() : null}</p>
+    </>
   )
 }
 
