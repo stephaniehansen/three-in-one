6667905f07b5206a9788638a3c6ed57e7ac0f3fe
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-19 17:21:53 +0100

    feat: round temperature to nearest integer

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -5,10 +5,10 @@ const WeatherCard = ({ city }) => {
   return (
     <>
       <h2>{city.name}</h2>
-      <p>Temperature: {city.main.temp}</p>
+      <p>Temperature: {city.main.temp.toFixed()}</p>
       <p>Humidity: {city.main.humidity}</p>
-      <p>Current Min Temperature: {city.main.temp_max}</p>
-      <p>Current Max Temperature: {city.main.temp_min}</p>
+      <p>Current Min Temperature: {city.main.temp_max.toFixed()}</p>
+      <p>Current Max Temperature: {city.main.temp_min.toFixed()}</p>
     </>
   )
 }
