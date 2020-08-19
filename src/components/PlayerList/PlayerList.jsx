b15183cc6c8890f7b77de84021210adbb051f299
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-19 22:09:14 +0100

    feat: group players by position

--- src/components/PlayerList/PlayerList.jsx
+++ src/components/PlayerList/PlayerList.jsx
@@ -4,16 +4,33 @@ import styles from "./PlayerList.module.scss";
 import PlayerCard from "../PlayerCard";
 
 const PlayerList = ({ players }) => {
-
-  const getPlayers = () => {
-    return players.map(player => <PlayerCard key={player.id} player={player} />)
+  const getPlayers = (position) => {
+    return players.filter(player => player.position === position)
+    .map(player => <PlayerCard key={player.id} player={player} />)
   }
 
   return (
-    <>
+    <section className={styles.playerList}>
       <h1>Football Team Organiser</h1>
-      {getPlayers()}
-    </>
+      <div className={styles.playersContainer}>
+        <div>
+          <h2>Defenders</h2>
+          {getPlayers("Defender")}
+        </div>
+        <div>
+          <h2>Midfielders</h2>
+          {getPlayers("Midfielder")}
+        </div>
+        <div>
+          <h2>Attackers</h2>
+          {getPlayers("Attacker")}
+        </div>
+        <div>
+          <h2>Goalkeepers</h2>
+          {getPlayers("Goalkeeper")}
+        </div>
+      </div>
+    </section>
   )
 }
 
