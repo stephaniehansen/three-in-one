84e7ad12a82c9596030fdf9c222ed167aaf34c45
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-19 21:10:31 +0100

    feat: display players from data file

--- src/components/PlayerList/PlayerList.jsx
+++ src/components/PlayerList/PlayerList.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import styles from "./PlayerList.module.scss";
+
+import PlayerCard from "../PlayerCard";
 
 const PlayerList = ({ players }) => {
+
+  const getPlayers = () => {
+    return players.map(player => <PlayerCard key={player.id} player={player} />)
+  }
+
   return (
-    <p>PlayerList works</p>
+    <>
+      <h1>Football Team Organiser</h1>
+      {getPlayers()}
+    </>
   )
 }
 
