b3e6b834d932f9526b75284432468886488ae76f
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-21 00:41:06 +0100

    feat: update Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import { Link } from "@reach/router";
+import { Card } from '@material-ui/core';
 
-const Main = ({ weather }) => {
+import weather from "../../assets/weather-img.jpeg";
+import quiz from "../../assets/quiz-img.jpg";
+import football from "../../assets/football-team.jpg";
+
+import styles from "./Main.module.scss"
+
+const Main = () => {
   return (
-    <>
-      <Link to="/weather">
-        <h2>Weather</h2>
-      </Link>
-      <Link to="/quiz">
-        <h2>One Question Quiz</h2>
-      </Link>
-      <Link to="/team-organiser">
-        <h2>Football Team Organiser</h2>
-      </Link>
-    </>
+    <section className={styles.main}>
+      <h1>Welcome</h1>
+      <div className={styles.mainContainer}>
+        <Card className={styles.mainCard} variant="outlined">
+          <Link to="/weather">
+            <img src={weather} alt="Weather" />
+            <div className={styles.cardContent}>
+              <h2>Weather</h2>
+              <p>Displays current weather information for London, Bristol and Rome.</p>
+            </div>
+          </Link>
+        </Card>
+        <Card className={styles.mainCard} variant="outlined">
+          <Link to="/quiz">
+            <img src={quiz} alt="One Question Quiz" />
+            <div className={styles.cardContent}>
+              <h2>One Question Quiz</h2>
+              <p>See if you can correctly answer the most difficult quiz in the world.</p>
+            </div>        
+          </Link>
+        </Card>
+        <Card className={styles.mainCard} variant="outlined">
+          <Link to="/team-organiser">
+          <img src={football} alt="Football" />
+            <div className={styles.cardContent}>
+              <h2>Team Organiser</h2>
+              <p>Create a team of your ideal Borussia Mönchengladbach starting eleven.</p>
+            </div>
+          </Link>
+        </Card>
+      </div>
+    </section>
   )
 }
 
