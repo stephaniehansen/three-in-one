ab0d229e3065f91fa6a2e1431a95973e89acab4e
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 19:14:12 +0100

    feat: add material-ui components

--- src/components/PlayerCard/PlayerCard.jsx
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import styles from "./PlayerCard.module.scss";
 
+import { 
+  Checkbox, 
+  ListItem, 
+  ListItemSecondaryAction, 
+  ListItemText, 
+  ListItemAvatar, 
+  Avatar } from '@material-ui/core';
+
 const PlayerCard = ({ player, handleChecked, isFull }) => {
   const [isChecked, setIsChecked] = useState(false);
 
@@ -10,10 +18,23 @@ const PlayerCard = ({ player, handleChecked, isFull }) => {
   }
 
   return (
-    <span>
-      <label htmlFor={player.id}>{player.name}</label>
-      <input type="checkbox" name={player.position} id={player.id} value={player.name} onClick={(e) => toggleCheckbox(e)} disabled={!isChecked && isFull}/>
-    </span>
+    <div className={styles.playerCard}>      
+      <ListItem button>
+        <ListItemAvatar>
+          <Avatar
+            src={`https://d2mt4yaynhqpf8.cloudfront.net/player/lars-stindl-borussia-moenchengladbach-dfl-sea-0001k3-500x500.png`}
+          />
+        </ListItemAvatar>
+        <ListItemText>{player.name}</ListItemText>
+        <ListItemSecondaryAction>
+        <Checkbox 
+          value={player.name} 
+          onClick={(e) => toggleCheckbox(e)} 
+          disabled={!isChecked && isFull} 
+        />
+        </ListItemSecondaryAction>
+      </ListItem>
+    </div>
   )
 }
 
