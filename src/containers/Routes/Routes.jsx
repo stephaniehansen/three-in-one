1e284d3a1013c4dfebce28d4456aa23cc3adb06a
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 17:37:08 +0100

    feat: update Routes, component paths

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Router } from '@reach/router';
 
 import Main from "../../components/Main";
+import WeatherList from "../../components/WeatherList";
+import Quiz from "../../components/Quiz";
+import PlayerList from "../../components/PlayerList";
 
-const Routes = () => {
+const Routes = ({ weather }) => {
   return (
     <Router>
       <Main path="/" />
+      <WeatherList path="/weather" weather={weather} />
+      <Quiz path="/quiz" />
+      <PlayerList path="/football" />
     </Router>
   );
 }
