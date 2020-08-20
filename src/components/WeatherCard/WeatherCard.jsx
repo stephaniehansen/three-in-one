6562cb268119f2caae71f7fc0629978abc0bfcf5
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-21 00:41:35 +0100

    feat: add further structure, material-ui card

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Card } from '@material-ui/core';
+
 import styles from "./WeatherCard.module.scss";
 
 const WeatherCard = ({ city }) => {
   return (
-    <>
+    <Card className={styles.weatherCard} variant="outlined">
       <h2>{city.name}</h2>
-      <p>Temperature: {city.main.temp.toFixed()}&deg;</p>
-      <p>Humidity: {city.main.humidity}</p>
-      <p>Current Min Temperature: {city.main.temp_max.toFixed()}&deg;</p>
-      <p>Current Max Temperature: {city.main.temp_min.toFixed()}&deg;</p>
-    </>
+      <div className={styles.cardContent}>
+        <p>Temperature: {city.main.temp.toFixed()}&deg;</p>
+        <p>Humidity: {city.main.humidity}%</p>
+        <p>Current Min Temperature: {city.main.temp_max.toFixed()}&deg;</p>
+        <p>Current Max Temperature: {city.main.temp_min.toFixed()}&deg;</p>
+      </div>
+    </Card>
   )
 }
 
