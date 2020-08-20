75589e7f5b54f4f15a2c401a08eef526c9df17df
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 14:29:55 +0100

    feat: display player name, add checkbox

--- src/components/PlayerCard/PlayerCard.jsx
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from "./PlayerCard.module.scss";
 
-const PlayerCard = () => {
+const PlayerCard = ({ player, handleChecked, isFull }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggleCheckbox = (e) => {
+    setIsChecked(!isChecked);
+    handleChecked(e, player.position);
+  }
+
   return (
-    <p>PlayerCard works</p>
+    <span>
+      <label htmlFor={player.id}>{player.name}</label>
+      <input type="checkbox" name={player.position} id={player.id} value={player.name} onClick={(e) => toggleCheckbox(e)} disabled={!isChecked && isFull}/>
+    </span>
   )
 }
 
