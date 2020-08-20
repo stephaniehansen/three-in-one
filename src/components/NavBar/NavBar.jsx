90f3beb6c250aa972b54bb5f80020fd59d7f8ca4
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 21:17:53 +0100

    feat: add nav structure and links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from "@reach/router";
+import styles from "./NavBar.module.scss";
+
+import { AppBar, Toolbar, Button } from '@material-ui/core';
+import BubbleChartIcon from '@material-ui/icons/BubbleChart';
 
 const NavBar = () => {
   return (
-    <p>NavBar works</p>
+    <nav className={styles.navBar}>
+      <AppBar position="static">
+        <Toolbar className={styles.toolbar}>
+          <div className={styles.logo}>
+            <Link to="/">
+              <BubbleChartIcon fontSize="large"/>
+            </Link>
+          </div>
+          <div>
+            <Link to="/weather">
+              <Button color="inherit">Weather</Button>
+            </Link>
+            <Link to="/quiz">
+              <Button color="inherit">Quiz</Button>
+            </Link>
+            <Link to="/team-organiser">
+              <Button color="inherit">Team Organiser</Button>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </nav>
   )
 }
 
