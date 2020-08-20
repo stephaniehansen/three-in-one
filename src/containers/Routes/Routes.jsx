140ca650cf0d2c8029aa03f154d77085d6df7ea2
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 17:08:47 +0100

    feat: add Routes component

---
+++ src/containers/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router } from '@reach/router';
+
+const Routes = () => {
+  return (
+    <Router>
+      <p>Router works</p>
+    </Router>
+    );
+}
+
+export default Routes;
