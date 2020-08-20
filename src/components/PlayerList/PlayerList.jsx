4bdd973ccdb407eec78d10aee765c016a5708957
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 15:06:05 +0100

    feat: display remaining count for positions

--- src/components/PlayerList/PlayerList.jsx
+++ src/components/PlayerList/PlayerList.jsx
@@ -5,6 +5,7 @@ import team from "../../data/team";
 import PlayerCard from "../PlayerCard";
 
 const PlayerList = ({ players }) => {
+  const limit = { "Defender": 3, "Midfielder": 4, "Attacker": 3, "Goalkeeper": 1 }
   const [selectedPlayers, setSelectedPlayers] = useState(team);
 
   const getPlayers = (position) => {
@@ -14,8 +15,12 @@ const PlayerList = ({ players }) => {
   }
 
   const checkFull = (position) => {
-    const limit = { "Defender": 3, "Midfielder": 4, "Attacker": 3, "Goalkeeper": 1 }
     return Object.keys(selectedPlayers[position]).length >= limit[position] ? true : false
+  }
+
+  const getRemaining = (position) => {
+    const count = limit[position] - (selectedPlayers[position]).length;
+    return count > 0 ? `Select ${count} ${position}s:` : `${position}s Full`;
   }
 
   const handleChecked = (e, position) => {
@@ -32,18 +37,22 @@ const PlayerList = ({ players }) => {
       <div className={styles.playersContainer}>
         <article>
           <h2>Defenders</h2>
+          <span>{getRemaining("Defender")} </span>
           {getPlayers("Defender")}
         </article>
         <article>
           <h2>Midfielders</h2>
+          <span>{getRemaining("Midfielder")} </span>
           {getPlayers("Midfielder")}
         </article>
         <article>
           <h2>Attackers</h2>
+          <span>{getRemaining("Attacker")} </span>
           {getPlayers("Attacker")}
         </article>
         <article>
           <h2>Goalkeepers</h2>
+          <span>{getRemaining("Goalkeeper")} </span>
           {getPlayers("Goalkeeper")}
         </article>
       </div>
