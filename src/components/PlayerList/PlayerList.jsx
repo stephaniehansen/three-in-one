b10b9939513320f6f88cc4169abec19e6030d1eb
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 03:50:20 +0100

    feat: update selectedPlayers on player selection

--- src/components/PlayerList/PlayerList.jsx
+++ src/components/PlayerList/PlayerList.jsx
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./PlayerList.module.scss";
 
 import PlayerCard from "../PlayerCard";
 
 const PlayerList = ({ players }) => {
+  const team = {
+    "Defender": [],
+    "Midfielder": [],
+    "Attacker": [],
+    "Goalkeeper": []
+  }
+  const [selectedPlayers, setSelectedPlayers] = useState(team);
+
   const getPlayers = (position) => {
     return players.filter(player => player.position === position)
-    .map(player => <PlayerCard key={player.id} player={player} />)
+    .map(player => 
+      <PlayerCard key={player.id} player={player} handleChecked={handleChecked} />)
   }
 
+  const handleChecked = (player) => {
+    setSelectedPlayers({
+      ...selectedPlayers,
+      [player.position]:  [...selectedPlayers[player.position], {name: player.name}]
+    });
+  }
+  
   return (
     <section className={styles.playerList}>
       <h1>Football Team Organiser</h1>
       <div className={styles.playersContainer}>
-        <div>
+        <article>
           <h2>Defenders</h2>
           {getPlayers("Defender")}
-        </div>
-        <div>
+        </article>
+        <article>
           <h2>Midfielders</h2>
           {getPlayers("Midfielder")}
-        </div>
-        <div>
+        </article>
+        <article>
           <h2>Attackers</h2>
           {getPlayers("Attacker")}
-        </div>
-        <div>
+        </article>
+        <article>
           <h2>Goalkeepers</h2>
           {getPlayers("Goalkeeper")}
-        </div>
+        </article>
       </div>
+      {console.log(selectedPlayers)}
     </section>
   )
 }
