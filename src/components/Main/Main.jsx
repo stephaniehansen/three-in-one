d9afc16e2ce056500c9b3ff957266fdeb69b928c
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-20 17:38:40 +0100

    feat: add links for weather, quiz, team organiser

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from "@reach/router";
 
 const Main = ({ weather }) => {
   return (
-    <p>Main works</p>
+    <>
+      <Link to="/weather">
+        <h2>Weather</h2>
+      </Link>
+      <Link to="/quiz">
+        <h2>One Question Quiz</h2>
+      </Link>
+      <Link to="/team-organiser">
+        <h2>Football Team Organiser</h2>
+      </Link>
+    </>
   )
 }
 
